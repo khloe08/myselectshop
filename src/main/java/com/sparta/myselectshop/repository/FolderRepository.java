f3ba203ed3b5007353bfc0f2f0dbe1660f78e6f5
khloe08 <owlnae04@gmail.com>
2025-01-19 21:36:26 +0900

    상품 폴더 설계

---
+++ src/main/java/com/sparta/myselectshop/repository/FolderRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.myselectshop.repository;
+
+import com.sparta.myselectshop.entity.Folder;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FolderRepository extends CrudRepository<Folder, Long> {
+}
---
+++ src/main/java/com/sparta/myselectshop/repository/ProductFolderRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.myselectshop.repository;
+
+import com.sparta.myselectshop.entity.ProductFolder;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductFolderRepository extends CrudRepository<ProductFolder, Long> {
+}
